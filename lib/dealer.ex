13b6e119029b395a2088eebb946aace44dbceee3
Will Hudson <williamhudson1218@gmail.com>
2021-05-13 20:44:28 -0600

    added docs to get_reviews function

--- lib/dealer.ex
+++ lib/dealer.ex
@@ -1,6 +1,21 @@
 defmodule Dealer do
   alias Dealer.Reviews
 
+  @doc """
+  Get's reviews from dealerrater.com
+  Parses reviews into %Review{} struct
+  Calculates the score for each review
+  Prints the scores to console
+
+  Returns [:ok]
+
+  ## Examples
+
+      iex> Dealer.get_reviews()
+      :ok
+
+  """
+  @spec get_reviews :: :ok
   def get_reviews() do
     IO.puts("Fetching Reviews...")
     Reviews.get_reviews()
